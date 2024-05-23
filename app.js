95593b23f558dc5062a5f91986129b65446ea772
clapsheep <sktndid1203@naver.com>
2024-05-23 17:59:10 +0900

    [http] 요청 받은 Data 처리하기 - form에 입력해서 전송하기 - 요청한 Data 전송받기 - fs를 사용해 Data의 내용을 담은 파일 생성하기 - node.js의 요청, 응답에 대한 과정 학습 - stream, Buffer 학습

--- app.js
+++ app.js
@@ -1,8 +1,48 @@
 const http = require("http");
+const fs = require("fs");
 
 const server = http.createServer((req, res) => {
-  // HTMl을 수동으로 서버에 입력해서 넣어주는 실습
+  const url = req.url;
+  const method = req.method;
 
+  // url이 '/'일 때 반환할 HTML
+  if (url === "/") {
+    res.write("<html>");
+    res.write("<head><title>My First Page</title></head>");
+
+    // form 태그가 submit이 되면 action의 경로로 이동하며 POST 메서드를 사용해 제출함.
+    res.write(
+      "<body><form action='/message' method='POST'><input name='message' type='text'><button type='submit'>Send</button></form></body>"
+    );
+    res.write("</html>");
+    return res.end();
+  }
+
+  // /message 경로에서 method로 POST를 받았을 때 생겨나는 일.
+  if (url === "/message" && method === "POST") {
+    const body = [];
+
+    // node.js가 stream으로 요청을 처리할 때, buffer를 할당해 chunk를 처리하는 방법
+
+    // data 이벤트가 발생할 때, chunk를 처리함,
+    req.on("data", (chunk) => {
+      console.log("chunk:", chunk);
+      body.push(chunk);
+    });
+
+    // 그 후 새로운 Buffer를 생성해 정의된 body를 담음
+    req.on("end", () => {
+      const parsedBody = Buffer.concat(body).toString();
+      const message = parsedBody.split("=")[1];
+      // 파일생성
+      fs.writeFileSync("message.txt", message);
+    });
+
+    res.statusCode = 302;
+    // 경로 이동
+    res.setHeader("Location", "/");
+    return res.end();
+  }
   res.setHeader("Content-Type", "text/html");
   res.write("<html>");
   res.write("<head><title>My First Page</title></head>");
