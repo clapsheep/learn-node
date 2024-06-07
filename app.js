27893e79855b7279d93752860ec8550754aa66c5
clapsheep <sktndid1203@naver.com>
2024-06-08 05:51:49 +0900

    [express] 미들웨어 학습 - 미들웨어 use 학습 - 3개의 파라미터를 가짐 - 1, 2는 req, res - 3은 next로 다음 미들웨어로 요청이 이동하기 위해 실행해야하는 함수이다.

--- app.js
+++ app.js
@@ -1,6 +1,15 @@
 const http = require("http");
-const routes = require("./routes");
-const server = http.createServer(routes.handler);
-console.log(routes.someText);
+const express = require("express");
+
+const app = express();
+
+app.use((req, res, next) => {
+  console.log("In the middleware");
+  next(); // 다음 미들웨어로 요청이 이동하려면 해당 next 함수를 실행해야한다.
+});
+app.use((req, res, next) => {
+  console.log("In another middleware");
+});
+const server = http.createServer(app);
 
 server.listen(3000);
