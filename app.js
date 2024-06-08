81aeb37c68d415bef811b22a178defb92c2faa93
clapsheep <sktndid1203@naver.com>
2024-06-08 17:14:23 +0900

    [express] post에만 작동하기
    - use를 사용하지 않고 post를 사용함 (get통신의 경우 get을 사용함)
    - 이는 url에 /product를 입력하면 post가 아닌 get으로 진입했기 때문에 redirect되지 않는 것을 확인할 수 있음

--- app.js
+++ app.js
@@ -9,16 +9,16 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use("/add-product", (req, res, next) => {
   res.send(
     '<form action="/product" method="POST"><input type="text" name="title"><button type="submit">Add Product</button></form>'
-  ); // 서버로 부터 html을 응답 받음
+  );
 });
 
-app.use("/product", (req, res, next) => {
+app.post("/product", (req, res, next) => {
   console.log(req.body);
   res.redirect("/");
 });
 
 app.use("/", (req, res, next) => {
-  res.send("<h1>Hello from Express!</h1>"); // 서버로 부터 html을 응답 받음
+  res.send("<h1>Hello from Express!</h1>");
 });
 
 app.listen(3000);
